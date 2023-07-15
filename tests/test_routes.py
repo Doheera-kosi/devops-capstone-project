133a899d492fba5e8f51e502156042309fe2b93d
Doheera-kosi <kupourevans99@gmail.com>
2023-07-15 10:18:08 -0400

    added test for delete account and fix failed test for delete account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -149,7 +149,7 @@ class TestAccountService(TestCase):
         self.assertEqual(len(data), 5)
     
 
-    #UPDATE ACCOUNT
+    # UPDATE ACCOUNT
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -164,3 +164,17 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
+    
+
+    # DELETE ACCOUNT
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+    
+    # METHOD NOT ALLOWED
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
